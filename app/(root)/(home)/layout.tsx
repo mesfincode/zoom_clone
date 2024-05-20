1f5cbdd90a5b14130eda8798798f2e9c75673162
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-05-20 17:20:57 +0300

    implemented the hero section

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -9,10 +9,8 @@ const HomeLayout = ({children}:{children:React.ReactNode}) => {
 
     <div className='flex'>
         <Sidebar /> 
-        <section className='flex min-h-screen flex-1 flex-col px-6 sm:px-14 pb-6 max-md:pb-14 pt-28  '>
-         <div className='w-full'>
-         {children}
-         </div>
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
         </section>
     </div>
   
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingTypeList = () => {
+  return (
+    <div>
+      <h1>Meeting</h1>
+    </div>
+  )
+}
+
+export default MeetingTypeList
