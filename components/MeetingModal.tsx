c491ac61f9252d3db45b691eb4b86ab5f74c66c5
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-05-22 15:39:22 +0300

    create scheduled meeting

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -10,32 +10,56 @@ import {
 import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
 interface modalProps {
     isOpen: boolean;
     title: string;
     handleClose: () => void;
-    className: string;
-    buttonText: string;
+    className?: string;
+    buttonText?: string;
+    buttonIcon?: string;
     handleClick: () => void;
     loading: boolean;
+    image?: string;
+    children?: ReactNode;
+
 }
-const MeetingModal = ({ isOpen, handleClose, title, className, handleClick, buttonText,loading }: modalProps) => {
+const MeetingModal = ({ isOpen, handleClose, title, className, handleClick, buttonText, loading, image, children ,buttonIcon}: modalProps) => {
     return (
         <Dialog open={isOpen} onOpenChange={handleClose}>
             <DialogContent className='bg-dark-1 text-white flex flex-col w-full max-w-[520px]
             gap-6 border-none px-6 py-9 
             '>
-        <div className="flex flex-col gap-6">
-        <h1 className={cn('text-3xl font-bold leading-[42px]',className)}> { title}</h1>
-                <Button variant="outline" onClick={handleClick} className='bg-blue-1 border-none text-white  focus-visible:ring-0
-                focus-visiible:ring-offset-0 
-                '
-                disabled={loading}
-                >{
-                    loading? <Loader2/>:buttonText
-                }</Button>
-        </div>
+                <div className="flex flex-col gap-6">
+                    {image && (
+                        <div className="flex justify-center">
+                            <Image src={image} alt="checked" width={72} height={72} />
+                        </div>
+                    )}
+                    <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+                        {title}
+                    </h1>
+                    {children}
+                    <Button
+
+                        className={
+                            "bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+                        }
+                        onClick={handleClick}
+                    >
+                        {buttonIcon && (
+                            <Image
+                                src={buttonIcon}
+                                alt="button icon"
+                                width={13}
+                                height={13}
+                            />
+                        )}{" "}
+                        &nbsp;
+                        {buttonText || "Schedule Meeting"}
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
 
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,10 +8,13 @@ import { useUser } from '@clerk/nextjs'
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { useToast } from "@/components/ui/use-toast"
 import { ToastAction } from './ui/toast'
+import { Textarea } from './ui/textarea'
+import ReactDatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
 const MeetingTypeList = () => {
   const { toast } = useToast()
-  const [loading, setLoading ] = useState(false);
+  const [loading, setLoading] = useState(false);
   const route = useRouter();
   const [meetingState, setMeetingState] = useState<'isScheduledMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
   const { user } = useUser()
@@ -42,33 +45,92 @@ const MeetingTypeList = () => {
       })
       setCallDetail(call);
       if (!values.description) {
-        route.push(`/meeting/${call.id}`)
+        route.push(`/meeting/${call.id}`);
       }
       toast({
-      
-        title: "Meating created successfully",
-        // description: "Friday, February 10, 2023 at 5:57 PM",
-        // action: (
-        //   <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-        // ),
-      })
+        title: 'Meeting Created',
+      });
 
     } catch (error) {
       setLoading(false)
 
-      toast({title:"Error occured",variant:"destructive"})
+      toast({ title: "Error occured", variant: "destructive" })
       console.log(error)
-    }finally{
+    } finally {
       setLoading(false);
     }
   }
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetail?.id}`;
+
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-      <HomeCard handleClick={() => setMeetingState('isScheduledMeeting')} img='/icons/add-meeting.svg' title='New Meeting' className='bg-blue-1' description='Start instant meeting now' />
+      <HomeCard handleClick={() => setMeetingState('isInstantMeeting')} img='/icons/add-meeting.svg' title='New Meeting' className='bg-blue-1' description='Start instant meeting now' />
+      <HomeCard handleClick={() => setMeetingState('isJoiningMeeting')} img='/icons/add-meeting.svg' title='Join Meeting' className='bg-yellow-1' description='Via invitation link' />
+
       <HomeCard handleClick={() => setMeetingState('isScheduledMeeting')} img='/icons/schedule.svg' title='Schedule Meeting' className='bg-purple-1' description='Plan  your meeting' />
       <HomeCard handleClick={() => route.push('/recordings')} img='/icons/recordings.svg' title='View Recordings' className='bg-orange-1' description='Check out your recordings' />
-      <HomeCard handleClick={() => setMeetingState('isJoiningMeeting')} img='/icons/add-meeting.svg' title='Join Meeting' className='bg-yellow-1' description='Via invitation link' />
-      <MeetingModal loading={loading} isOpen={meetingState != undefined} handleClose={() => setMeetingState(undefined)} title="Start a new meeting" buttonText='Start Meeting' className='text-center' handleClick={createMeeting} />
+      
+      {
+        !callDetail?(
+          <MeetingModal
+          loading={loading}
+          isOpen={meetingState === 'isScheduledMeeting'}
+          handleClose={() => setMeetingState(undefined)}
+          title="Create meeting"
+          handleClick={createMeeting}
+          buttonText='Schedule Meeting'
+        >
+          <div className='flex flex-col gap-2.5'>
+            <label className='text-base text-normal leading-[22px] text-sky-2'>
+               Add a description
+            </label>
+            <Textarea 
+                 className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+                 onChange={(e) =>
+                   setValues({ ...values, description: e.target.value })
+                 }
+            />
+
+          </div>
+          <div className='flex w-full flex-col gap-2.5'>
+            <label className='text-base text-normal leading-[22px] text-sky-2'>Select Date and Time</label>
+              <ReactDatePicker selected={values.dateTime}
+              onChange={(date)=>setValues({...values,dateTime:date!})}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+              />
+          </div>
+        </MeetingModal>
+        ):(
+          <MeetingModal
+          loading={loading}
+          isOpen={meetingState === "isScheduledMeeting"}
+          handleClose={() => setMeetingState(undefined)}
+          title="Meeting Created"
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: 'Link Copied' });
+          }}
+          image={'/icons/checked.svg'}
+          buttonIcon="/icons/copy.svg"
+          className="text-center"
+          buttonText="Copy Meeting Link"       
+        />
+        )
+      }
+      <MeetingModal
+        loading={loading}
+        isOpen={meetingState === 'isInstantMeeting'}
+        handleClose={() => setMeetingState(undefined)}
+        title="Start a new meeting"
+        buttonText='Start Meeting'
+        className='text-center'
+        handleClick={createMeeting}
+      />
 
     </section>
   )
